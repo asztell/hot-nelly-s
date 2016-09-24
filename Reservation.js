db92878ad119dc06434ea0535ded7d739a052f2b
Arpad Asztalos <arpad.web@gmail.com>
2016-09-24 11:10:30 -0700

    added routes to express

--- Reservation.js
+++ Reservation.js
@@ -32,5 +32,13 @@ var customer = [{
 // Basic route that sends the user first to the AJAX Page
 app.get('/', function (req, res) {
 	// res.send('Welcome to the Star Wars Page!')
-	res.sendFile(path.join(__dirname, 'view.html'));
+	res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.get('/reserve', function (req, res) {
+	res.sendFile(path.join(__dirname, 'reserve.html'));
+});
+
+app.get('/tables', function (req, res) {
+	res.sendFile(path.join(__dirname, 'tables.html'));
 });
